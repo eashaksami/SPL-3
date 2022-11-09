42bd39cce896c3015d3deee13093156033662968
sami <eashaksami0176@gmail.com>
2022-11-09 13:53:25 +0600

    git repo clone code added

--- bug_localizer/src/main/java/com/example/bug_localizer/controller/BugLocalizerController.java
+++ bug_localizer/src/main/java/com/example/bug_localizer/controller/BugLocalizerController.java
@@ -1,11 +1,11 @@
 package com.example.bug_localizer.controller;
 
+import com.example.bug_localizer.service.GitCloneService;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -23,9 +23,12 @@ public class BugLocalizerController {
     // define a location
     public static final String DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
 
+    @Autowired
+    GitCloneService gitCloneService;
+
     // Define a method to upload files
     @PostMapping("/upload")
-    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files") List<MultipartFile> multipartFiles) throws IOException, IOException {
+    public ResponseEntity<List<String>> uploadFiles(@RequestParam("files") List<MultipartFile> multipartFiles) throws IOException {
         List<String> filenames = new ArrayList<>();
         System.out.println(multipartFiles);
         for(MultipartFile file : multipartFiles) {
@@ -35,5 +38,11 @@ public class BugLocalizerController {
             filenames.add(filename);
         }
         return ResponseEntity.ok().body(filenames);
+    }
+
+    @PostMapping(value = "/clone-repo/{repoLink}")
+    public ResponseEntity<String> cloneGitRepo(@PathVariable(value = "repoLink") String repoLink) throws GitAPIException, IOException {
+        gitCloneService.cloneRepository(repoLink);
+        return ResponseEntity.ok().body("Repo cloned successfully!!!");
     }
 }
---
+++ bug_localizer/src/main/java/com/example/bug_localizer/service/GitCloneService.java
@@ -0,0 +1,54 @@
+package com.example.bug_localizer.service;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GitCloneService {
+    public void cloneRepository(String repoLink) throws GitAPIException, IOException {
+
+        File dir = createTempDirectory();
+
+        try(
+                Git result = Git.cloneRepository()
+                        .setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
+                        .setURI(repoLink)
+                        .setDirectory(new File(dir.getAbsolutePath())).call()
+        ) {
+            result.getRepository().close();
+        }
+
+        printAllFileList(dir.getAbsolutePath());
+    }
+
+    public File createTempDirectory() {
+        String workingDirPath = "/home/sami/Desktop/SPL-3/Temp";
+
+        File workingDirectory = new File(workingDirPath);
+
+        File dir = new File(workingDirectory, "newDir");
+        dir.mkdirs();
+
+        return dir;
+    }
+
+    public void printAllFileList(String filePath) throws IOException {
+        List<File> filesInFolder = Files.walk(Paths.get(filePath))
+                .filter(Files::isRegularFile)
+                .map(Path::toFile)
+                .collect(Collectors.toList());
+
+        System.out.println(filesInFolder);
+    }
+}
